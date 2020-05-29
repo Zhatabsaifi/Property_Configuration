89b30c801c0beb77d92e33b6e109d9965d1df05f
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-05-30 01:06:01 +0530

    fetch data in envirnment variable .it is used for fetch data from differnt operating system. Closes #1.

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+    @Autowired
+    Environment env;
+
     @Value("${saifi.username}")   // spring expression language
     String username;
 
@@ -22,7 +27,8 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(username);
+        fakeDataSource.setUsername(env.getProperty("USERNAME"));
+       // fakeDataSource.setUsername(username);
         fakeDataSource.setPassword(password);
         fakeDataSource.setDburl(url);
         return fakeDataSource;
